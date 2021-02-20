38cafcc47f13a5499152d5a5f4408ada4995fa8c
SUHAIL AHMAD PARRAY <37875320+suhailparray98@users.noreply.github.com>
2021-02-21 00:59:53 +0530

    CALCULATE A RAISED POWER B  WITH C PROGRAM
    CALCULATE A RAISED POWER B  WITH C PROGRAM WHERE A AND B CAN BE ANY NUMBERS

---
+++ FIND_POWER_OF ANY_NUMBER.c
@@ -0,0 +1,15 @@
+/*C PROGRAM TO FIND THE POWER OF ANY NUMBER */
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;//defining and intializing variable
+    printf("enter the number you want to find power:");
+    scanf("%d",&a);
+    printf("enter raised power number : ");
+    scanf("%d",&b);
+    c= pow(a,b);//using power math function to calculate the b power 3 i.e 2 power 3 which is equal to 8
+    printf("2 raised power 3 is :%d",c);//displaying the output to the console
+    return 0;
+}
+//code contributed by SUHAIL AHMAD PARRAY
